b185ded1e596aac53e30303420b0e057f0658192
yaNick811 <167251929+yaNick811@users.noreply.github.com>
2024-05-13 16:26:27 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,17 @@
+def test(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+
+
+# Пример использования функции
+test(1, 2, 3, name='Nikita', age=22)
+
+
+def summa(n):
+    if n == 0:
+        return 0
+    else:
+        return n + summa(n - 2)
+
+print(summa(14))
+
